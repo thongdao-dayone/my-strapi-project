c19bb115cfa2a260ff69dc1de42fc4a7118fbd6b
Dao Duy Thong <duythong28112004@gmail.com>
2024-12-03 16:44:24 +0700

    first commit

--- config/database.ts
+++ config/database.ts
@@ -7,7 +7,7 @@ export default ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      schema: env("DATABASE_SCHEMA", "public"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: { 
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
